396080f3dfa9ecfc562387da84b09062c0de7fff
Sandro Reis <consultorsandro@hotmail.com>
2025-04-18 09:25:55 -0300

    Aula 40

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#[allow(unused_variables)]
+fn main() {
+    //Aula 44 Integers
+    let sixteen_bit_signed: i16 = -32500;
+    let sixteen_bit_unsigned: u16 = 64000;
+
+    let thirty_two_bit_signed = -21474836;
+    let thirty_two_bit_unsigned: u32 = 4294697295;
+    let some_value = 20i8;
+}
