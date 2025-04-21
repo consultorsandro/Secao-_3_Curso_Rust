1d5d552fc99261799151003c3af0a66c1dc0c298
Sandro Reis <consultorsandro@hotmail.com>
2025-04-21 08:31:23 -0300

    Class 48

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 #[allow(unused_variables)]
 fn main() {
-    //Aula 44 Integers
+    //Class 48
+    let value: i32 = -15;
+    println!("{}", value.abs());
+
+    let empty_space = "        my content   ";
+    println!("{}", empty_space.trim());
+
+    
+
+
+
+    
+}
+/*
+//Aula 44 Integers
     let sixteen_bit_signed: i16 = -32500;
     let sixteen_bit_unsigned: u16 = 64000;
 
     let thirty_two_bit_signed = -21474836;
     let thirty_two_bit_unsigned: u32 = 4294697295;
     let some_value = 20i8;
-}
+*/
